7cf112e0842ee5e2e97f5c45f4eabe85a03fcafe
Wsz980 <Wensen980@gmail.com>
2017-12-05 17:20:04 -0700

    wrote some bugs

--- src/org/usfirst/frc/team4585/robot/Robot.java
+++ src/org/usfirst/frc/team4585/robot/Robot.java
@@ -1,23 +1,33 @@
 package org.usfirst.frc.team4585.robot;
 
 import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.smartdashboard.*;
 import org.usfirst.frc.team4585.robot.TankDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.AnalogInput;
 
 public class Robot extends SampleRobot {
+	final int MILLISPERITERATION = 5;
 	//DrivePorts
 	int driveLPort = 8;
 	int driveRPort = 9;
-	//Joystic
+	//Joystick
 	int joystickPort = 0;
 	//Claw
-	
+	int clawPort = 7;
 	//PingPongShooter
 	
 	
-	//Initilization of robot functions
-	TankDrive chassis = new TankDrive(driveLPort, driveRPort);
+	//Initialization of robot functions
 	Extreme3DPro joy = new Extreme3DPro(joystickPort);
-
+	TankDrive chassis = new TankDrive(driveLPort, driveRPort, joy);
+	PingPongShooter shooter = new PingPongShooter(joy);
+	RoboClaw claw = new RoboClaw(clawPort);
+	
+	GhostController Marcus = new GhostController(chassis, claw);
+	AnalogInput JunkAnalog = new AnalogInput(2);
+	SmartDashboard JunkDash = new SmartDashboard();
+	
 	public Robot() {
 		
 	}
@@ -29,21 +39,37 @@ public class Robot extends SampleRobot {
 	
 	@Override
 	public void autonomous() {
-	
+		final long AUTONMOUS_TIME_MILLI = 600;
+		long time = System.currentTimeMillis();
+		long ExpireTime = time + AUTONMOUS_TIME_MILLI;
+
+		int CycleCounter = 0;
+		Marcus.Reset();
+		while(isEnabled() && (time < ExpireTime)){
+		
+			if(System.currentTimeMillis() >= (time + MILLISPERITERATION)){
+				Marcus.AutonomousControl();
+				time = System.currentTimeMillis();
+			}
+		}
+		claw.move(false, false);
 	}
 
 	
 	//Loop checking for robot movement
 	@Override
 	public void operatorControl() {
+
 		long time;
-		final int MILLISPERITERATION = 5;
 		time = System.currentTimeMillis();
 		while(isEnabled() & isOperatorControl()) {
 			if (System.currentTimeMillis() >= time + MILLISPERITERATION) {
+				JunkDash.putDouble("Voltage", JunkAnalog.getVoltage());
 				
-				chassis.arcadeDrive(-joy.getZ(), joy.getY());
 				
+				chassis.arcadeDrive();
+				shooter.operatorControl();
+				claw.move(joy.getButton(5),joy.getButton(3));
 				time = System.currentTimeMillis();
 				
 			}
--- src/org/usfirst/frc/team4585/robot/TankDrive.java
+++ src/org/usfirst/frc/team4585/robot/TankDrive.java
@@ -1,21 +1,32 @@
 package org.usfirst.frc.team4585.robot;
 
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class TankDrive {
 	private RobotDrive chassis;
-
+	private Extreme3DPro _joy;
+	private AnalogGyro _gyro;
+	SmartDashboard dash = new SmartDashboard();
+	
+	private final double ROTATIONCONSTANT = 1;
 	private final double SCALEX = 1;
 	private final double SCALEY = 2;
-	private final double DEADX = .10;
+	private final double DEADX = .5;
 	private final double DEADY = .10;
 
-	public TankDrive(int MOTORLEFT, int MOTORRIGHT) {
+	public TankDrive(int MOTORLEFT, int MOTORRIGHT, Extreme3DPro joyparam) {
 		chassis = new RobotDrive(MOTORLEFT, MOTORRIGHT);
 		chassis.setExpiration(.1);
+		_joy = joyparam;
+		_gyro = new AnalogGyro(1);
 	}
 
-	public void arcadeDrive(double magX, double magY) { // x is rotation, y is forward/back
+	public void arcadeDrive() { // x is rotation, y is forward/back
+		double magX = -_joy.getZ();
+		double magY = _joy.getY();
+		
 		magX = applyDeadzone(magX, DEADX);
 		magY = applyDeadzone(magY, DEADY);
 
@@ -37,6 +48,27 @@ public class TankDrive {
 	private double applyScale(double input, double power) {
 		return Math.copySign(Math.pow(input, power), input);
 
+	}
+	
+	public void Spin4While(){
+		chassis.arcadeDrive(0, 1);
+	}
+	
+	public void MoveSlowly(){
+		chassis.arcadeDrive(0.5, ROTATIONCONSTANT * _gyro.getAngle());
+		dash.putDouble("GyroAngle", _gyro.getAngle());
+	}
+	
+	public void MoveBackSlowly(){
+		chassis.arcadeDrive(-0.5, ROTATIONCONSTANT * _gyro.getAngle());
+	}
+	
+	public void Reset(){
+		_gyro.reset();
+	}
+	
+	public void doNothing(){
+		dash.putDouble("GyroAngle", _gyro.getAngle());
 	}
 
 }
